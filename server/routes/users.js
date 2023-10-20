9d5a395bf95fe3368e8a2fa2311f73eb3b5c3a43
Fernando Marturet <fernando.martu@gmail.com>
2023-10-20 10:20:23 -0400

    fixing issue with friends list not loading

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,10 @@
 import express from "express";
 
 import {
-    getUser,
-    getUserFriends,
-    addRemoveFriend
-} from "../controllers/users.js"
+  getUser,
+  getUserFriends,
+  addRemoveFriend,
+} from "../controllers/users.js";
 
 import { verifyToken } from "../middleware/auth.js";
 
@@ -12,8 +12,8 @@ const router = express.Router();
 
 /* READ */
 
-router.get("/:id", verifyToken, getUser)
-router.get("/:id", verifyToken, getUserFriends)
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
 
 /* UPDATE */
 router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
